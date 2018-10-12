86497cd1216f9e84b10090270ae9366c6e36b09c
Ludovic Noirault <ludovic.noirault@hotmail.fr>
2018-10-12 20:56:10 +0200

    Nearly ready

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,23 +1,64 @@
-var clicked = false;
-
 function openSide() {
 
     var burgerElem = document.getElementById("burger");
     var sideElem = document.getElementById("side");
-    var sideMain = document.getElementById("main");
+    var mainElem = document.getElementById("main");
 
     console.log("click dammit !")
 
-    if (clicked === true) {
+    if (sideElem.style.left === "-17rem") {
         sideElem.style.left = "0";
-        sideMain.style.width = "62vw"
         burgerElem.style.left = "17.5rem";
-        clicked = false;
+        mainElem.style.width = "67%";
+
     } else {
         sideElem.style.left = "-17rem";
-        sideMain.style.width = "95vw";
         burgerElem.style.left = "0.5rem";
-        burgerElem.style.opacity = "1";
-        clicked = true;
+        mainElem.style.width = "95%";
     }
 };
+
+var counter = 0;
+
+function slide(arrow) {
+    var arrowPressed = document.getElementById(arrow);
+
+    var leftArrow = document.getElementById("leftArrow");
+    var rightArrow = document.getElementById("rightArrow");
+
+    var elemProjects = document.getElementById("projects");
+    var listProjects = elemProjects.children;
+    var project;
+
+    /* handle the counter */
+    if (arrowPressed === leftArrow) {
+        counter = counter - 1;
+    } else {
+        counter = counter + 1;
+    };
+
+    console.log('Current counter', counter);
+
+    project = listProjects[counter];
+    project.style.zIndex = 0;
+
+    /*handle visibility of the arrows*/
+    if (counter != 0) {
+        leftArrow.style.visibility = "visible";
+    } else {
+        leftArrow.style.visibility = "hidden";
+    };
+
+    if (counter === (listProjects.length - 1)) {
+        rightArrow.style.visibility = "hidden";
+    } else {
+        rightArrow.style.visibility = "visible";
+    };
+
+    for (var i = 0; i < listProjects.length; i++) {
+        listProjects[i].style.zIndex = 0;
+    }
+
+    project.style.left = 0;
+    project.style.zIndex = 1;
+}
